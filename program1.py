6a9d0c4b9674672bc0d8b7c821d3a4e0e0c3f15b
Nabin Kumar Mandal <nmandal@lululemon.com>
2026-03-29 11:46:20 +0530

    First commit

---
+++ program1.py
@@ -0,0 +1,68 @@
+'''
+
+name=input("What is your name? ")
+age=int(input("How old are you? "))
+print(f"Hello {name} !! Your are {age} years old.")
+print(f"Next Year you will be {age+1} !!")
+
+number=int(input("Enter a number"))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+'''
+
+'''
+Calculator program
+'''
+
+'''
+number1=int(input("Enter thr First Number- "))
+number2=int(input("Enter the Second Number- "))
+
+print(f"Addition: {number1+number2}")
+print(f"Subtraction: {number1-number2}")
+print(f"Multiplication: {number1*number2}")
+
+if number2 == 0:
+    print("Division is not possible. Number2 is zero.")
+else:
+    print(f"Division: {number1/number2}")
+'''
+
+''' Grade Program '''
+'''
+marks=input("Enter the Marks - ")
+
+if not(marks.isdigit()):
+    print("Invalid marks. please enter number only")
+else:
+    marks=int(marks)
+
+    if marks > 100 or marks < 0:
+        print("Invalid marks Entered.")
+    elif marks >= 90:
+        print("Grade A")
+    elif marks >= 80:
+        print("Grade B")
+    elif marks >= 50:
+        print("Grade C")
+    else:
+        print("FAIL")
+
+'''
+
+'''
+Loop
+'''
+
+n=int(input("Enter a Number: "))
+
+total = 0
+
+for i in range(1,n+1):
+    total = total + i
+
+print(f"sum = {total}")
